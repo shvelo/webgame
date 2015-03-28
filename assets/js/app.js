e6c32e98361e229ac89cd7ae52effeb5ce97c072
Nick Shvelidze <captain@pirrate.me>
2015-03-28 23:31:04 +0400

    Woah

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+angular.module('app', [])
+.controller('appCtrl', function($scope, $interval){
+    $scope.items = {
+        bagel: {
+            name: "bagel",
+            price: 10,
+            have: 0
+        }
+    };
+    $scope.buy = function(item){
+        var i = $scope.items[item];
+        if(i.available) {
+            $scope.coins -= i.price;
+            i.have += 1;
+        }
+    }
+    $scope.drop = function(){
+        $scope.coins = 0;
+    }
+    $scope.coins = 0;
+    $scope.coinRate = 1;
+
+    var step = function(){
+        $scope.coins += $scope.coinRate;
+        for(item in $scope.items) {
+            var i = $scope.items[item];
+            if($scope.coins >= i.price) i.available = true;
+        }
+    }
+
+    $interval(function(){
+        step();
+    }, 500);
+});
